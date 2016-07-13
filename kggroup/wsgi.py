10caa6e084e67929db5a0d486e72010718e5fbdb
end-e <xiuliang555@foxmail.com>
2016-07-13 10:32:53 +0800

    配置apache

--- kggroup/wsgi.py
+++ kggroup/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+sys.path.insert(0, BASE_DIR)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kggroup.settings")
 
 application = get_wsgi_application()
