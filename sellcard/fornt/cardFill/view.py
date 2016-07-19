513a099d5cfcbbd3f32bb54c62e4004ce5e750cb
lbf1121 <lbf1121@gmail.com>
2016-07-19 13:34:47 +0800

    补卡

--- sellcard/fornt/cardFill/view.py
+++ sellcard/fornt/cardFill/view.py
@@ -1,4 +1,10 @@
 #-*- coding:utf-8 -*-
 from django.shortcuts import render
+
 def index(reauest):
+
     return render(reauest,'cardFill.html',locals())
+
+def query(reauest):
+
+    return render(reauest,'cardFillQuery.html',locals())
--- sellcard/urls.py
+++ sellcard/urls.py
@@ -4,7 +4,7 @@ urlpatterns = [
     url(r'^sellcard/cardsale/','sellcard.fornt.cardSale.view.index',name='cardsale'),
     url(r'^sellcard/cardssale/','sellcard.fornt.cardsSale.view.index',name='cardssale'),
     url(r'^sellcard/cardchange/','sellcard.fornt.cardChange.view.index',name='cardchange'),
-    url(r'^sellcard/cardfill/','sellcard.fornt.cardFill.view.index',name='cardfill'),
+    #url(r'^sellcard/cardfill/','sellcard.fornt.cardFill.view.index',name='cardfill'),
     url(r'^sellcard/queryfill/','sellcard.fornt.bulkSale.view.queryFill',name='queryfill'),
     url(r'^sellcard/bulksale/','sellcard.fornt.bulkSale.view.index',name='bulksale'),
     url(r'^sellcard/cardsent/', 'sellcard.fornt.cardSent.view.index',name='cardsent'),
@@ -20,4 +20,7 @@ urlpatterns = [
     url(r'^sellcard/cardcheck/', 'sellcard.common.Method.cardCheck',name='cardcheck'),
 
     url(r'^sellcard/setSn/', 'sellcard.common.Method.setOrderSn',name='setSn'),
+
+    url(r'^sellcard/cardfill/index/','sellcard.fornt.cardFill.view.index',name='cardfill_index'),
+    url(r'^sellcard/cardfill/query/','sellcard.fornt.cardFill.view.query',name='cardfill_query'),
 ]
