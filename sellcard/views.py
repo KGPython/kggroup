51c6bbda6efab807c606f32dae8dded38ce6e218
lbf1121 <lbf1121@gmail.com>
2016-07-25 09:33:45 +0800

    添加加密锁

--- sellcard/views.py
+++ sellcard/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 def index(request):
 
     return render(request, "index.html")
+
+def global_setting(request):
+    # 加密狗验证服务地址
+    SOFTKEY_URL = "http://192.168.250.8:8082/authservice/inf/main"
+    return locals()
