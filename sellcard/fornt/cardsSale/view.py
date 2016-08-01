1e5f38bca21e76d04f7ecef14c4ae7ee7b1cac92
chenxinguang1003 <583860027@qq.com>
2016-08-01 14:36:27 +0800

    1、折扣四舍五入到元 2、添加优惠返现

--- sellcard/fornt/cardsSale/view.py
+++ sellcard/fornt/cardsSale/view.py
@@ -15,28 +15,6 @@ def index(request):
     roleid= request.session.get("s_roleid",'')
     rates = request.session.get('s_rates')
 
-
-        # conn = mtu.getMysqlConn()
-        # sql = 'SELECT card_no,card_value,card_status,card_blance FROM card_inventory ' \
-        #       'WHERE card_no >='+cardStart+' and card_no <='+cardEnd+' ORDER BY card_no'
-        # cur = conn.cursor()
-        # cur.execute(sql)
-        # cardList = cur.fetchall()
-        # conn.close()
-        #
-        # cardTotalVal = 0.00
-        # cardTotalNum = 0
-        # for card in cardList:
-        #     if card['card_status'] == '1' and card['card_value']==card['card_blance']:
-        #         cardTotalVal += float(card['card_value'])
-        #         cardTotalNum += 1
-        # rate=0
-        # discountVal=0
-        # for obj in rates:
-        #     if obj['val_min']<=cardTotalVal and obj['val_max']>=cardTotalVal:
-        #         rate = obj['discount_rate']*100
-        #         discountVal = cardTotalVal*rate/100
-
     return render(request,'cardsSale.html',locals())
 
 @csrf_exempt
