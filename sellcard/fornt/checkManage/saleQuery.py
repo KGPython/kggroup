eed799f898503aaaf4a91cbb9e68e226d8d95d4d
chenxinguang1003 <583860027@qq.com>
2016-10-15 10:47:54 +0800

    1、修改销售明细查询，销售人员查询选项的bug

--- sellcard/fornt/checkManage/saleQuery.py
+++ sellcard/fornt/checkManage/saleQuery.py
@@ -16,50 +16,56 @@ def index(request):
     shops = base.findShop()
     departs = base.findDepart()
     today = str(datetime.date.today())
-    if role_id=='2':
-        shop = mth.getReqVal(request,'shop',shopcode)
-    if role_id =='1' or role_id=='6':
-        shop = mth.getReqVal(request,'shop','')
-    depart = mth.getReqVal(request,'depart','')
-    name = mth.getReqVal(request,'operator','').strip()
-    operator =''
-    if name:
-        user = AdminUser.objects.values('id').filter(name=name)
-        operator = user[0]['id']
+    resList=[]
 
+    #接收post查询
+    depart = mth.getReqVal(request,'depart','')
     actionType = mth.getReqVal(request,'actionType','1')
     start = mth.getReqVal(request,'start',today)
     end = mth.getReqVal(request,'end',today)
     endTime = datetime.datetime.strptime(end,'%Y-%m-%d') + datetime.timedelta(1)
-    page = mth.getReqVal(request,'page',1)
+    name = mth.getReqVal(request,'operator','').strip()
+    operator =''
+    if name:
+        user = AdminUser.objects.values('id').filter(name=name)
+        if user:
+            operator = user[0]['id']
+            if role_id=='2':
+                shop = shopcode
+            if role_id =='1' or role_id=='6':
+                shop = mth.getReqVal(request,'shop','')
 
-    resList=[]
-    kwargs = {}
-    if shop:
-        kwargs.setdefault('shop_code',shop)
-    if operator:
-        kwargs.setdefault('operator_id',operator)
-    if depart:
-        kwargs.setdefault('depart',depart)
-    kwargs.setdefault('add_time__gte',start)
-    kwargs.setdefault('add_time__lte',endTime)
-    if actionType=='1':
-        resList = Orders.objects.values('shop_code','depart','operator_id','order_sn','action_type','paid_amount','disc_amount','add_time')\
-                .filter(**kwargs)\
-                .order_by('shop_code','depart','operator_id')
-    elif actionType=='2':
-        resList = OrderUpCard.objects.values('shop_code','depart','operator_id','order_sn','diff_price','user_name','user_phone','modified_time')\
-                .filter(**kwargs)\
-                .order_by('shop_code','depart','operator_id')
-    elif actionType=='3':
-        resList = OrderChangeCard.objects.values('shop_code','depart','operator_id','order_sn','total_in_price','total_out_price','add_time')\
-                .filter(**kwargs)\
-                .order_by('shop_code','depart','operator_id')
+            page = mth.getReqVal(request,'page',1)
 
-    paginator = Paginator(resList,20)
-    try:
-        resList = paginator.page(page)
-    except Exception as e:
-        print(e)
-    return  render(request,'saleQuery.html',locals())
+            kwargs = {}
+            if shop:
+                kwargs.setdefault('shop_code',shop)
+            if operator:
+                kwargs.setdefault('operator_id',operator)
+            if depart:
+                kwargs.setdefault('depart',depart)
+            kwargs.setdefault('add_time__gte',start)
+            kwargs.setdefault('add_time__lte',endTime)
+            if actionType=='1':
+                resList = Orders.objects.values('shop_code','depart','operator_id','order_sn','action_type','paid_amount','disc_amount','add_time')\
+                        .filter(**kwargs)\
+                        .order_by('shop_code','depart','operator_id')
+            elif actionType=='2':
+                resList = OrderUpCard.objects.values('shop_code','depart','operator_id','order_sn','diff_price','user_name','user_phone','modified_time')\
+                        .filter(**kwargs)\
+                        .order_by('shop_code','depart','operator_id')
+            elif actionType=='3':
+                resList = OrderChangeCard.objects.values('shop_code','depart','operator_id','order_sn','total_in_price','total_out_price','add_time')\
+                        .filter(**kwargs)\
+                        .order_by('shop_code','depart','operator_id')
+
+            paginator = Paginator(resList,20)
+            try:
+                resList = paginator.page(page)
+            except Exception as e:
+                print(e)
+        else:
+            operator = name
+
+    return render(request,'saleQuery.html',locals())
 
