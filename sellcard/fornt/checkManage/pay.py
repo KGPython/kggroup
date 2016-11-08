0b5e21d6b4abccaceb20531f76138bb1baaa21b0
ChenXinGuang1003 <583860027@qq.com>
2016-11-08 15:59:28 +0800

    1、发卡，去空格

--- sellcard/fornt/checkManage/pay.py
+++ sellcard/fornt/checkManage/pay.py
@@ -7,16 +7,22 @@ import datetime
 from sellcard.common import Method as mth
 @csrf_exempt
 def index(request):
+    shop = request.session.get('s_shopcode')
+    role = request.session.get('s_roleid')
+    user = request.session.get('s_uid')
+
     if request.method == 'POST':
         start = request.POST.get('start','')
         end = request.POST.get('end','')
         endTime = datetime.datetime.strptime(end,'%Y-%m-%d').date() + datetime.timedelta(1)
+        whereStr ="WHERE a.add_time >='"+start+"' AND a.add_time <='"+str(endTime)+"' AND a.order_sn = b.order_id "
+
+        if role in ('1', '2'):
+            whereStr += " AND a.shop_code ='"+shop+"'"
         conn = mth.getMysqlConn()
         cur = conn.cursor()
 
-        sql = "SELECT b.pay_id,SUM(b.pay_value) AS pay_value FROM orders as a,order_payment_info as b " \
-              "WHERE a.add_time >='"+start+"' AND a.add_time <='"+str(endTime)+"' AND a.order_sn = b.order_id " \
-              "GROUP BY b.pay_id ORDER BY b.pay_id"
+        sql = "SELECT b.pay_id,SUM(b.pay_value) AS pay_value FROM orders as a,order_payment_info as b "+whereStr+"GROUP BY b.pay_id ORDER BY b.pay_id"
         cur.execute(sql)
         payList = cur.fetchall()
 
@@ -29,5 +35,4 @@ def index(request):
         payTotal = 0.00
         for row in payList:
             payTotal += float(row['pay_value'])
-
     return render(request,'payCheck.html',locals())
