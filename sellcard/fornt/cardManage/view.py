211f390368704d7c386797eb9c58d5d5df9c9d11
ChenXinGuang1003 <583860027@qq.com>
2016-11-17 14:09:00 +0800

    1、卡入库修改

--- sellcard/fornt/cardManage/view.py
+++ sellcard/fornt/cardManage/view.py
@@ -6,6 +6,7 @@ import datetime,json
 
 from sellcard.common import Method as m
 from sellcard.models import ActionLog,CardInventory
+from sellcard.common.model import MyError
 
 # 卡入库
 @csrf_exempt
@@ -32,21 +33,23 @@ def cardInStore(request):
                     res['msg']='1'
                 else:
                     sql="SELECT CardNO,detail FROM guest WHERE SheetID ='"+sheetid+"'"
-
                     cur.execute(sql)
                     cardList = cur.fetchall()
                     conn.close()
                     try:
-                        for card in cardList:
-                            CardInventory.objects.filter(card_no=card['CardNO'])\
-                                    .update(card_blance =card['detail'],card_value =card['detail'],charge_time=datetime.datetime.now(),sheetid=sheetid)
-                            card['detail'] = float(card['detail'])
-                        res['msg']='2'
-                        ActionLog.objects.create(action='门店卡入库',u_name=request.session.get('s_uname'),cards_in=json.dumps(cardList),add_time=datetime.datetime.now())
+                        with transaction.atomic():
+                            for card in cardList:
+                                updateNum = CardInventory.objects.filter(card_no=card['CardNO'],card_status='1',card_action='1',card_blance=0)\
+                                        .update(card_blance =card['detail'],card_value =card['detail'],charge_time=datetime.datetime.now(),sheetid=sheetid)
+                                card['detail'] = float(card['detail'])
+                                if not updateNum:
+                                    raise MyError(card['CardNO']+'状态更新失败')
+                            res['msg']='2'
+                            ActionLog.objects.create(action='门店卡入库',u_name=request.session.get('s_uname'),cards_in=json.dumps(cardList),add_time=datetime.datetime.now())
 
                     except Exception as e:
                         print(e)
                         res['msg']='3'
-                        ActionLog.objects.create(action='门店卡入库',u_name=request.session.get('s_uname'),cards_in=json.dumps(cardList),add_time=datetime.datetime.now(),err_msg=e)
+                        ActionLog.objects.create(action='门店卡入库',u_name=request.session.get('s_uname'),add_time=datetime.datetime.now(),err_msg=e)
 
     return render(request,'cardInStore.html',locals())
