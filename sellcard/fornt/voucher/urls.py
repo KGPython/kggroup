1bdc7a24fb7160e8f1bcf95334a5bf9aa174893b
ChenXinGuang1003 <583860027@qq.com>
2016-12-23 16:40:24 +0800

    1、分离卡和卷

---
+++ sellcard/fornt/voucher/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import url
+
+urlpatterns = [
+
+    # 发行代金券
+    # 列表
+    url(r'^voucher/issue/$', 'sellcard.fornt.voucher.issue.index', name="voucherIssueList"),
+    # 创建
+    url(r'^voucher/issue/create/$',
+        'sellcard.fornt.voucher.issue.create', name="voucherIssueCreate"),
+    # 打印
+    url(r'^voucher/issue/printed/$',
+        'sellcard.fornt.voucher.issue.printed', name="voucherIssuePrint"),
+    # 核销代金券
+    # 主页
+    url(r'^voucher/balance/$', 'sellcard.fornt.voucher.balance.index', name="voucherBalance"),
+
+
+
+]
