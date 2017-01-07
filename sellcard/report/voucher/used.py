90fe495161a148c7895d4bd942dcc4656bccddaa
QX <2393035935@qq.com>
2017-01-07 19:41:00 +0800

    增加代金券财务报表

--- sellcard/report/voucher/used.py
+++ sellcard/report/voucher/used.py
@@ -13,20 +13,6 @@ def index(request):
     cardType = mth.getReqVal(request,'cardtype','')
     page = mth.getReqVal(request,'page',1)
 
-    cardList = CardInventory.objects.values('card_no','card_value','card_blance','card_status','charge_time','sheetid')\
-            .filter(card_blance=cardType,shop_code=shopCode,card_status='1',card_action='1',card_blance__gt='0')\
-            .order_by('card_no')
 
-    totalBalance = 0.00
-    totalNum = 0
-    for row in cardList:
-        totalBalance += float(row['card_blance'])
-        totalNum +=1
-
-    paginator = Paginator(cardList,20)
-    try:
-        cardList = paginator.page(page)
-    except Exception as e:
-        print(e)
 
     return render(request, 'report/stockGroupByCardNo.html', locals())
