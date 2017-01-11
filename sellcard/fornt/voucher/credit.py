d4d167e042637fec5ddea80aff90ae4f0a340ea2
QX <2393035935@qq.com>
2017-01-11 15:05:49 +0800

    修改代金券名称为购物券

--- sellcard/fornt/voucher/credit.py
+++ sellcard/fornt/voucher/credit.py
@@ -49,7 +49,7 @@ def index(request):
 
     List = KfJobsCoupon.objects.values('shop_code', 'coupon_code', 'create_user_name',
          'type', 'batch', 'start_date', 'end_date', 'values', 'range', 'pay_account',
-          'credit_account').filter(**kwargs,credit_account__lt=F('pay_account')).order_by('create_date')
+          'credit_account').filter(credit_account__lt=F('pay_account'), **kwargs).order_by('create_date')
 
     # 表单分页开始
     paginator = Paginator(List, 8)
