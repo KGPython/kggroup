20f95be1078c8fdeb5c23f986309c26723e41487
QX <2393035935@qq.com>
2017-01-17 22:16:55 +0800

    修改代金券名称为购物券

--- sellcard/report/voucher/urls.py
+++ sellcard/report/voucher/urls.py
@@ -1,10 +1,16 @@
 __author__ = 'qixu'
 from django.conf.urls import url
 urlpatterns = [
-    #代金券销售汇总（财务用）
+    #购物券销售汇总（财务用）
     url(r'^payment/$', 'sellcard.report.voucher.payment.index', name="voucherPayment"),
-    #代金券库存盘点
+    #购物券库存盘点
+    #列表
     url(r'^stock/$', 'sellcard.report.voucher.stock.index', name="voucherStock"),
-    #代金券核销情况
+    # 使用明细
+    url(r'^stock/detail/$', 'sellcard.report.voucher.stock.detail', name="voucherStockDetail"),
+    #购物券核销情况
+    # 列表
     url(r'^used/$', 'sellcard.report.voucher.used.index', name="voucherUsed"),
+    # 批次明细
+    url(r'^used/detail/$', 'sellcard.report.voucher.used.detail', name="voucherUsedDetail"),
 ]
