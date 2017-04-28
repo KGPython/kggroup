4458423c1f127b8623405fd1c5470d5a14ab6693
QX <2393035935@qq.com>
2017-04-28 15:19:29 +0800

    大客户

--- sellcard/fornt/card/cardManage/cancel.py
+++ sellcard/fornt/card/cardManage/cancel.py
@@ -33,7 +33,7 @@ def index(request):
 
 
 def save(request):
-    u_name = request.session.get('s_uname')
+    u_name = request.session.get('s_unameChinese')
     s_uid = request.session.get('s_uid')
     # 检测session中Token值，判断用户提交动作是否合法
     Token = request.session.get('postToken', default=None)
@@ -44,13 +44,26 @@ def save(request):
 
     card_no = request.POST.get('card_no')
     res = {}
-    conn = mth.getMssqlConn()
+    conn = mth.getMysqlConn()
     cur = conn.cursor()
     try:
         conn.autocommit(False)
-        sql = """insert into card_inventory_delete select ci.*, now(),"""+ s_uid + """,""" + u_name + """ from card_inventory ci WHERE ci.card_no = '""" + card_no + "'"
+        # sql = """insert into
+        #          card_inventory_delete
+        #          select ci.*, now(),"""+ s_uid + """,""" + u_name + """ from card_inventory ci WHERE ci.card_no = '""" + card_no + "'"
+
+        sql = u" insert into card_inventory_delete( " \
+              u" card_no,card_value,card_status,card_action,card_addtime,shop_code, " \
+              u" card_blance,charge_time,order_sn,sheetid,is_store,username, " \
+              u" delete_time,delete_user_id,delete_user_name)" \
+              u" select ci.card_no,ci.card_value,ci.card_status,ci.card_action,ci.card_addtime,ci.shop_code," \
+              u" ci.card_blance,ci.charge_time,ci.order_sn,ci.sheetid,ci.is_store,ci.username,now(), " \
+              u" '{s_uid}', " \
+              u" '{u_name}' " \
+              u" from card_inventory ci " \
+              u" WHERE ci.card_no = '{card_no}'".format(s_uid=s_uid,u_name=u_name,card_no=card_no)
         cur.execute(sql)
-        sql = """delete from card_inventory WHERE card_no = '""" + card_no + "'"
+        sql = u"delete from card_inventory WHERE card_no = '{card_no}'".format(card_no=card_no)
         cur.execute(sql)
         conn.commit()
         res['status'] = 1
