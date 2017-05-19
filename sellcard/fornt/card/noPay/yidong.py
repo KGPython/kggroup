875aed4c5ed7bcf09b2e5954ce914f92a9fded58
QX <2393035935@qq.com>
2017-05-19 09:23:07 +0800

    修改用户管理可用禁用状态

--- sellcard/fornt/card/noPay/yidong.py
+++ sellcard/fornt/card/noPay/yidong.py
@@ -14,11 +14,15 @@ def index(request):
         shop = request.session.get('s_shopcode', '')
         start = request.POST.get('start', monthFirstDay)
         end = request.POST.get('end', today)
+        order_sn = request.POST.get('order_sn', '')
 
         conn = mth.getMysqlConn()
         cur = conn.cursor()
         queryWhere1 = " and b.add_time >='{start} 00:00:00'  and b.add_time <='{end} 23:59:59' ".format(start=start,
                                                                                                         end=end)
+
+        if order_sn != '':
+            queryWhere1 += " and b.order_sn like '%{order_sn}%'".format(order_sn=order_sn)
         # 售卡数据
         sqlSale = "select a.pay_value,b.order_sn,b.operator_id,b.add_time,b.buyer_name,b.buyer_tel,b.paid_amount " \
                   "from order_payment_info as a ,orders as b " \
